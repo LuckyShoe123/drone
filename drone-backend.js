dd9b7f37ef2ff647365eaf49b7b55aae6186307d
LuckyShoe123 <hannah.paulding@icloud.com>
2015-07-24 10:19:36 +0100

    drone-version1

--- drone-backend.js
+++ drone-backend.js
@@ -8,14 +8,52 @@ Cylon.robot()
         adaptor: 'ardrone',
         port: '192.168.1.1'
     })
+
     .device("drone", {
         driver: "ardrone",
         connection: "ardrone"
     })
+
+    .device("nav",{
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
+
     .on("ready", fly);
     
 // Fly the bot
 function fly(robot) {
+    bot=robot;
+    bot.drone.config('general:navdata_demo', 'TRUE');
+    bot.nav.on("navdata", function(data){
+        //console.log(data);
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        // Drone is higher than 1.5 meters up
+        if (altitude > 1.5) {
+            bot.drone.land();
+        }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });
+
+    bot.drone.disableEmergency();
+
+    bot.drone.ftrim();
+
+    bot.drone.takeoff();
+
+    after(10*1000, function(){
+        bot.drone.land();
+    });
+
+    after(15*1000, function(){
+        bot.drone.stop();
+    });
 
 }
 
